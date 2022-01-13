f896381378fe166630d6d2f9a67c8387137206ac
onurkurtulmus <onurk.dev@outlook.com>
2022-01-13 11:40:00 +0300

    mongoose update 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,3 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_CONN_URL)
-
-
-
-// const me = new User({
-//     name: 'onurkdev',
-//     age: 28,
-//     email:'onurk.dev@gmail.com',
-//     password: 'pjosdasdasda'
-// })
-
-// me.save().then(()=> {
-// console.log(me)
-// }).catch((error)=> {
-// console.log('Error', error)
-// })
